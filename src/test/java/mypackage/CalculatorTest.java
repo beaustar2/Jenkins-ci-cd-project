f9c35cd3ad69c523d08ebbec692130e2f4266b26
beaustar2 <144544990+beaustar2@users.noreply.github.com>
2023-10-30 01:20:29 -0600

    Update CalculatorTest.java

--- src/test/java/mypackage/CalculatorTest.java
+++ src/test/java/mypackage/CalculatorTest.java
@@ -7,20 +7,25 @@ import static org.junit.Assert.assertThat;
 public class CalculatorTest {
     @Test
     public void twoAndThreeIsFive() throws Exception {
-        final long result = new Calculator().addFucn(2, 3);
+        final long result = new Calculator().addFunc(2, 3);
         assertThat(result, is(5L));
     }
 
     @Test
     public void threeMinusTwoIsOne() throws Exception {
-        final long result = new Calculator().subFucn(2, 3);
+        final long result = new Calculator().subFunc(3, 2);
         assertThat(result, is(1L));
     }
-    
+
     @Test
-    public void threeXThreeIsNine() throws Exception {
-        final long result = new Calculator().mulFucn(3, 3);
+    public void threeTimesThreeIsNine() throws Exception {
+        final long result = new Calculator().mulFunc(3, 3);
         assertThat(result, is(9L));
     }
-   
+
+    @Test
+    public void sixDividedByTwoIsThree() throws Exception {
+        final long result = new Calculator().divFunc(6, 2);
+        assertThat(result, is(3L));
+    }
 }
